198bac1fb7913fbb2b1bc4070073c5938fc30c9c
Slava <viacheslav.plishkin@gmail.com>
2023-02-16 14:34:02 +0200

    .env added to git

---
+++ docker_build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo chmod -R 0777 ./storage ./bootstrap/cache ./public
+sudo chown -R www-data ./storage ./bootstrap/cache ./public
+
+docker-compose build || docker-compose up -d
+
+docker-compose exec fpm composer dump-autoload
+docker-compose exec fpm php artisan optimize:clear
+docker-compose exec fpm php artisan migrate
