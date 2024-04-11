44b9be1ed49fb7679ab3e289988c8d10af5a9aa8
Viacheslav Plishkin <viacheslav.plishkin@gmail.com>
2024-04-11 10:50:27 +0300

    sail bootstrap/cache folder

--- docker_build.sh
+++ docker_build.sh
@@ -8,9 +8,16 @@ docker compose build
 docker compose up -d mysql
 sleep 5
 docker compose up -d laravel.test
+
+mkdir -p bootstrap/cache
+sudo chmod -R 0777 bootstrap/cache
+
 docker compose exec laravel.test composer install --optimize-autoloader --ignore-platform-reqs
+docker compose restart laravel.test
 ./vendor/bin/sail artisan config:clear
 ./vendor/bin/sail artisan migrate
 docker compose up -d
 ./vendor/bin/sail npm install
 ./vendor/bin/sail npm run build
+
+./vendor/bin/sail artisan app:update-currency
